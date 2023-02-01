e940184a3ab788939a6c203a3d412464b13534ff
mitjasha <mitjasha@gmail.com>
2023-02-02 01:09:28 +0300

    fix: file name

---
+++ src/pages/RegistrationScreen/RegistrationScreen.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./RegistrationScreen.scss";
+
+const RegistrationScreen: React.FC = () => {
+  return (
+    <div className="login-screen">
+      <div>Registration Screen</div>
+    </div>
+  );
+};
+export default RegistrationScreen;
