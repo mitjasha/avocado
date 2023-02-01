8d125d7c84f5b5a6c8cec5161e018c9be3e88d27
mitjasha <mitjasha@gmail.com>
2023-02-02 01:27:53 +0300

    feat: add basic button component

---
+++ src/components/Buttons/Button/Button.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "./Buttom.scss";
+
+export interface ButtonProps {
+  children?: React.ReactNode;
+  to?: string;
+  className?: string;
+  disabled?: boolean;
+  onClick?: () => void;
+}
+
+const Button: React.FC<ButtonProps> = ({
+  to,
+  children,
+  className,
+  disabled,
+  onClick,
+  ...attrs
+}) => {
+  const classes = `${"button"} ${className}`;
+  return (
+    <a href="/" rel="noopener noreferrer" className={classes} {...attrs}>
+      {children}
+    </a>
+  );
+};
+
+export default Button;
