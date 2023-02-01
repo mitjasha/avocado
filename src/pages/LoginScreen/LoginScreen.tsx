a0bbb9a455010cbfa80c1c8a3dd6793c0dd3a41f
mitjasha <mitjasha@gmail.com>
2023-02-02 01:01:02 +0300

    feat: add registration, login and splash screens

---
+++ src/pages/LoginScreen/LoginScreen.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./LoginScreen.scss";
+
+const LoginScreen: React.FC = () => {
+  return (
+    <div className="login-screen">
+      <div>Login Screen</div>
+    </div>
+  );
+};
+export default LoginScreen;
---
+++ src/pages/SplashScreen/SplashScreen.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./SplashScreen.scss";
+
+const SplashScreen: React.FC = () => {
+  return (
+    <div className="splash-screen">
+      <div>Splash Screen</div>
+    </div>
+  );
+};
+export default SplashScreen;
