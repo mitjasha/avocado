47d7575c1947beebfc4544dd40c402f1d09bad57
ViktoriaKondrashova1 <kavidmi@gmail.com>
2023-02-02 17:56:48 +0300

    feat: add header-container

--- src/containers/Header/Header.tsx
+++ src/containers/Header/Header.tsx
@@ -3,14 +3,16 @@ import "./Header.scss";
 
 const Header: React.FC = () => {
   return (
-    <header className="header container">
-      <input type="date" className="header__date-input" />
-      <div className="header__date">30 January, Monday</div>
-      <button type="button" className="header__menu-btn">
-        <div className="dot" />
-        <div className="dot" />
-        <div className="dot" />
-      </button>
+    <header className="header">
+      <div className="container header__container">
+        <input type="date" className="header__date-input" />
+        <div className="header__date">30 January, Monday</div>
+        <button type="button" className="header__menu-btn">
+          <div className="dot" />
+          <div className="dot" />
+          <div className="dot" />
+        </button>
+      </div>
     </header>
   );
 };
