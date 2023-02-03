84988dacc9c414226a71030655eb71277ab1f4bd
ViktoriaKondrashova1 <kavidmi@gmail.com>
2023-02-03 18:16:19 +0300

    feat: add daily meal events

--- src/pages/MainScreen/MainScreen.tsx
+++ src/pages/MainScreen/MainScreen.tsx
@@ -4,7 +4,25 @@ import "./MainScreen.scss";
 const MainScreen: React.FC = () => {
   return (
     <div className="main-screen">
-      <div>Main Screen</div>
+      <div className="container">
+        <div className="daily-events">
+          <div className="daily-meals">
+            <h3 className="daily-events__title">Daily meals</h3>
+            <div className="daily-events__meal daily-events__meal_breakfast">
+              <p className="meal-title meal-title_breakfast">Breakfast</p>
+            </div>
+            <div className="daily-events__meal daily-events__meal_lunch">
+              <p className="meal-title meal-title_lunch">Lunch</p>
+            </div>
+            <div className="daily-events__meal daily-events__meal_dinner">
+              <p className="meal-title meal-title_dinner">Dinner</p>
+            </div>
+            <div className="daily-events__meal daily-events__meal_snack">
+              <p className="meal-title meal-title_snack">Snack</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
