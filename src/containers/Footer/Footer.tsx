5123c03e343aaaf9ad872c798442fd59861b344a
ViktoriaKondrashova1 <kavidmi@gmail.com>
2023-02-03 15:06:38 +0300

    feat: add creators list

--- src/containers/Footer/Footer.tsx
+++ src/containers/Footer/Footer.tsx
@@ -3,8 +3,25 @@ import "./Footer.scss";
 
 const Footer: React.FC = () => {
   return (
-    <footer className="footer container">
-      <div>Footer</div>
+    <footer className="footer">
+      <div className="container footer__container">
+        <div className="footer__creators">
+          Creators
+          <ul className="creators-list">
+            <li className="list-item">
+              <a href="https://github.com/mitjasha">mitjasha</a>
+            </li>
+            <li className="list-item">
+              <a href="https://github.com/asya2320">asya2320</a>
+            </li>
+            <li className="list-item">
+              <a href="https://github.com/viktoriakondrashova1">
+                ViktoriaKondrashova1
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
     </footer>
   );
 };
