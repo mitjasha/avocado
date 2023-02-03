86dc90887927c7626ed922985312cc3b08fef417
ViktoriaKondrashova1 <kavidmi@gmail.com>
2023-02-03 18:34:17 +0300

    feat: add plus light icon and add-meal btns

--- src/pages/MainScreen/MainScreen.tsx
+++ src/pages/MainScreen/MainScreen.tsx
@@ -6,19 +6,35 @@ const MainScreen: React.FC = () => {
     <div className="main-screen">
       <div className="container">
         <div className="daily-events">
-          <div className="daily-meals">
+          <div className="daily-events__meals">
             <h3 className="daily-events__title">Daily meals</h3>
             <div className="daily-events__meal daily-events__meal_breakfast">
-              <p className="meal-title meal-title_breakfast">Breakfast</p>
+              <div className="meal-text">
+                <p className="meal-title meal-title_breakfast">Breakfast</p>
+                <p className="recom recom_breakfast">Recomended 447 Kcal</p>
+              </div>
+              <button type="button" className="add-meal" aria-label="plus" />
             </div>
             <div className="daily-events__meal daily-events__meal_lunch">
-              <p className="meal-title meal-title_lunch">Lunch</p>
+              <div className="meal-text">
+                <p className="meal-title meal-title_lunch">Lunch</p>
+                <p className="recom recom_lunch">Recomended 447 Kcal</p>
+              </div>
+              <button type="button" className="add-meal" aria-label="plus" />
             </div>
             <div className="daily-events__meal daily-events__meal_dinner">
-              <p className="meal-title meal-title_dinner">Dinner</p>
+              <div className="meal-text">
+                <p className="meal-title meal-title_dinner">Dinner</p>
+                <p className="recom recom_dinner">Recomended 447 Kcal</p>
+              </div>
+              <button type="button" className="add-meal" aria-label="plus" />
             </div>
             <div className="daily-events__meal daily-events__meal_snack">
-              <p className="meal-title meal-title_snack">Snack</p>
+              <div className="meal-text">
+                <p className="meal-title meal-title_snack">Snack</p>
+                <p className="recom recom_snack">Recomended 447 Kcal</p>
+              </div>
+              <button type="button" className="add-meal" aria-label="plus" />
             </div>
           </div>
         </div>
