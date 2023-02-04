8c3add88a8721ceb96211bf5ed0a354d7a476c1c
asya2320 <64478347+asya2320@users.noreply.github.com>
2023-02-04 17:07:41 +0300

    feat: add html structure for card recipe on main page

---
+++ src/components/CardRecipe/CardRecipe.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import "./CardRecipe.scss";
+
+export interface DataRecipes {
+  id: number;
+  name: string;
+  category: string;
+  calories: string;
+  proteins: string;
+  carbs: string;
+  fats: string;
+  author: string;
+  kitchen: string;
+  ingredients: { quantity: string; name: string; type: string }[];
+  steps: string[];
+  time: number;
+  imageURL: string;
+}
+
+interface CardRecipesProps {
+  data: DataRecipes;
+  // className?: string;
+}
+
+const CardRecipe: React.FC<CardRecipesProps> = ({ data }) => {
+  return (
+    <div>
+      <div style={{ backgroundImage: `url(${data.imageURL})` }} />
+      <div>
+        <h3>{data.name}</h3>
+        <div>
+          <div>
+            <div />
+            <span>{data.calories} Kcal</span>
+          </div>
+          <div>
+            <div />
+            <span>{data.time} min</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CardRecipe;
