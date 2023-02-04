71dae9d764e23f215f52dae741f0ff6364e2dc46
asya2320 <64478347+asya2320@users.noreply.github.com>
2023-02-05 00:44:13 +0300

    fix: remove commits

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
 import React from "react";
 import Footer from "./containers/Footer/Footer";
-// import Header from "./containers/Header/Header";
-// import RecipeScreen from "./pages/RecipeScreen/RecipeScreen";
-// import EventScreen from "./pages/EventScreen/EventScreen";
-// import MainScreen from "./pages/MainScreen/MainScreen";
-// import ProductCardScreen from "./pages/ProductCardScreen/ProductCardScreen";
-// import ProfileScreen from "./pages/ProfileScreen/ProfieScreen";
-// import RecipesScreen from "./pages/RecipesScreen/RecipesScreen";
+import Header from "./containers/Header/Header";
+import RecipeScreen from "./pages/RecipeScreen/RecipeScreen";
+import EventScreen from "./pages/EventScreen/EventScreen";
+import MainScreen from "./pages/MainScreen/MainScreen";
+import ProductCardScreen from "./pages/ProductCardScreen/ProductCardScreen";
+import ProfileScreen from "./pages/ProfileScreen/ProfieScreen";
+import RecipesScreen from "./pages/RecipesScreen/RecipesScreen";
 import CategoriesRecipesScreen from "./pages/CategoriesRecipesScreen/CategoriesRecipesScreen";
 
 const App: React.FC = () => {
   return (
     <>
       <CategoriesRecipesScreen />
-      {/* <Header />
+      <Header />
       <main className="main-app">
         <MainScreen />
         <EventScreen />
@@ -21,7 +21,7 @@ const App: React.FC = () => {
         <RecipesScreen />
         <ProductCardScreen />
         <ProfileScreen />
-      </main> */}
+      </main>
       <Footer />
     </>
   );
