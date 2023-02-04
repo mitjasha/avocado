34b034ef42e5aa1ec96f39e0a3c64ffbb3e223c9
asya2320 <64478347+asya2320@users.noreply.github.com>
2023-02-04 17:10:10 +0300

    feat: add scss styles for class recipe__card

--- src/components/CardRecipe/CardRecipe.tsx
+++ src/components/CardRecipe/CardRecipe.tsx
@@ -24,10 +24,13 @@ interface CardRecipesProps {
 
 const CardRecipe: React.FC<CardRecipesProps> = ({ data }) => {
   return (
-    <div>
-      <div style={{ backgroundImage: `url(${data.imageURL})` }} />
-      <div>
-        <h3>{data.name}</h3>
+    <div className="recipe__card">
+      <div
+        className="recipe__card__img"
+        style={{ backgroundImage: `url(${data.imageURL})` }}
+      />
+      <div className="recipe__card__wrapper">
+        <h3 className="recipe__card__h3">{data.name}</h3>
         <div>
           <div>
             <div />
