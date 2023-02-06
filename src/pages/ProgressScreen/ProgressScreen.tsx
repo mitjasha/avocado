4ca3794db341b762ef992dfe2b5db07f558239d3
ViktoriaKondrashova1 <kavidmi@gmail.com>
2023-02-06 19:56:48 +0300

    feat: add slash and current weight display

--- src/pages/ProgressScreen/ProgressScreen.tsx
+++ src/pages/ProgressScreen/ProgressScreen.tsx
@@ -15,17 +15,25 @@ const ProgressScreen: React.FC = () => {
   return (
     <div className="progress-screen">
       <h1 className="progress-screen__title">Your Progress</h1>
-      <div className="container">
-        <div className="progress-screen__chart">
-          <ChartComponent
-            chartData={[userWeight, kgLeft]}
-            colors={["#559C4F", "#FAFDF8"]}
-            size={153}
-            cutout={50}
-            spacing={3}
-          />
+      <div className="container progress-screen-container">
+        <div style={{ position: "relative" }}>
+          <div className="progress-screen__chart">
+            <ChartComponent
+              chartData={[userWeight, kgLeft]}
+              colors={["#559C4F", "#FAFDF8"]}
+              size={153}
+              cutout={50}
+              spacing={3}
+            />
+          </div>
+          <div className="progress-screen_kg-left">{kgLeft} kg left</div>
         </div>
-        <div className="progress-screen_kg-left">{kgLeft} kg left</div>
+        <div className="progress-screen__slash" />
+        <div className="progress-screen__curr-weight">
+          Current weight is{" "}
+          <span className="progress-screen__curr-weight_bg">{userWeight}</span>{" "}
+          kg
+        </div>
       </div>
     </div>
   );
