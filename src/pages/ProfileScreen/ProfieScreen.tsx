316092ec17ffe23839bcd08661fcb577b642c095
ViktoriaKondrashova1 <kavidmi@gmail.com>
2023-02-06 17:13:55 +0300

    feat: add female & male icons, add user heading

--- src/pages/ProfileScreen/ProfieScreen.tsx
+++ src/pages/ProfileScreen/ProfieScreen.tsx
@@ -1,10 +1,29 @@
 import React from "react";
+import female from "../../assets/png/female.png";
+import male from "../../assets/png/male.png";
 import "./ProfileScreen.scss";
 
 const ProfileScreen: React.FC = () => {
+  const userName = "Amanda";
+  const userGender = "female";
+  const userAvatar = userGender === "female" ? female : male;
   return (
     <div className="profile-screen">
-      <div>Profile Screen</div>
+      <div className="user-data">
+        <div className="container">
+          <div className="user-data__heading">
+            <img
+              src={userAvatar}
+              alt="user avatar"
+              className="user-data__heading__avatar"
+            />
+            <h1 className="user-data__heading__title">
+              Hello,
+              <br /> {userName}!
+            </h1>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
