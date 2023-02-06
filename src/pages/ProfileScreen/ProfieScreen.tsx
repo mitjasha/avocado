3768fe032947704550787f55b314a6b429e504a2
ViktoriaKondrashova1 <kavidmi@gmail.com>
2023-02-06 17:43:36 +0300

    feat: add user data

--- src/pages/ProfileScreen/ProfieScreen.tsx
+++ src/pages/ProfileScreen/ProfieScreen.tsx
@@ -4,9 +4,14 @@ import male from "../../assets/png/male.png";
 import "./ProfileScreen.scss";
 
 const ProfileScreen: React.FC = () => {
-  const userName = "Amanda";
-  const userGender = "female";
+  const userName = "Amanda" as string;
+  const userGender = "female" as string;
   const userAvatar = userGender === "female" ? female : male;
+  const userAge = 25 as number;
+  const userHeight = 167 as number;
+  const userWeight = 67 as number;
+  const userGoal = "lose weight" as string;
+  const userTargetWeight = 63 as number;
   return (
     <div className="profile-screen">
       <div className="user-data">
@@ -19,8 +24,26 @@ const ProfileScreen: React.FC = () => {
             />
             <h1 className="user-data__heading__title">
               Hello,
-              <br /> {userName}!
+              <br /> <span style={{ color: "#559c4f" }}>{userName}</span>!
             </h1>
+          </div>
+          <div className="user-data__info">
+            <div className="user-data__info__titles">
+              <p>Gender:</p>
+              <p>Age:</p>
+              <p>Height:</p>
+              <p>Weight:</p>
+              <p>Goal:</p>
+              <p>Target weight:</p>
+            </div>
+            <div className="user-data__info__answers">
+              <p>{userGender}</p>
+              <p>{userAge}</p>
+              <p>{userHeight} cm</p>
+              <p>{userWeight} kg</p>
+              <p>{userGoal}</p>
+              <p>{userTargetWeight} kg</p>
+            </div>
           </div>
         </div>
       </div>
