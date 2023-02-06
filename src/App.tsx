9cfe3b21303eb94c493b12fd423bcbf9359df35a
ViktoriaKondrashova1 <kavidmi@gmail.com>
2023-02-06 19:01:26 +0300

    fix: fix commented pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,21 @@
 import React from "react";
 import Footer from "./containers/Footer/Footer";
 import Header from "./containers/Header/Header";
-// import EventScreen from "./pages/EventScreen/EventScreen";
-// import MainScreen from "./pages/MainScreen/MainScreen";
-// import ProductCardScreen from "./pages/ProductCardScreen/ProductCardScreen";
+import EventScreen from "./pages/EventScreen/EventScreen";
+import MainScreen from "./pages/MainScreen/MainScreen";
+import ProductCardScreen from "./pages/ProductCardScreen/ProductCardScreen";
 import ProfileScreen from "./pages/ProfileScreen/ProfieScreen";
-// import RecipesScreen from "./pages/RecipesScreen/RecipesScreen";
+import RecipesScreen from "./pages/RecipesScreen/RecipesScreen";
 
 const App: React.FC = () => {
   return (
     <>
       <Header />
       <main className="main-app">
-        {/* <MainScreen />
+        <MainScreen />
         <EventScreen />
         <RecipesScreen />
-        <ProductCardScreen /> */}
+        <ProductCardScreen />
         <ProfileScreen />
       </main>
       <Footer />
