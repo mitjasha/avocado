04bb223baafe651ad04024323fe09297b708970f
ViktoriaKondrashova1 <kavidmi@gmail.com>
2023-02-06 19:05:36 +0300

    feat: add progress page

---
+++ src/pages/ProgressScreen/ProgressScreen.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./ProgressScreen.scss";
+
+const ProgressScreen: React.FC = () => {
+  return (
+    <div className="progress-screen">
+      <div>Progress Screen</div>
+    </div>
+  );
+};
+export default ProgressScreen;
