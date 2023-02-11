452ef0bba284bf8d340a66624b64101760cddc29
asya2320 <64478347+asya2320@users.noreply.github.com>
2023-02-11 14:25:01 +0300

    feat: add folder to edit event page

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ const App: React.FC = () => {
         <EventScreen type="Breakfast" />
         <RecipesScreen />
         <ProductCardScreen />
-        <ProfileScreen /> */}
+        <ProfileScreen />
         <RegistrationScreen />
       </main>
       <Footer />
---
+++ src/pages/EditEventsScreen/EventScreen/EditEventScreen.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "./EditEventScreen.scss";
+import "../../index.scss";
+
+const EditEventScreen = () => {
+  return <div>EditEventScreen</div>;
+};
+
+export default EditEventScreen;
