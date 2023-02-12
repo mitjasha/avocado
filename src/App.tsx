16987036c20af4318e3080d76c225c24967671a2
ViktoriaKondrashova1 <kavidmi@gmail.com>
2023-02-12 13:55:20 +0300

    feat: add login inputs, submit btn and adaptive

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
 import React from "react";
 import Footer from "./containers/Footer/Footer";
 import Header from "./containers/Header/Header";
-import RecipeScreen from "./pages/RecipeScreen/RecipeScreen";
-import RegistrationScreen from "./pages/RegistrationScreen/RegistrationScreen";
-import AboutUsScreen from "./pages/AboutUsScreen/AboutUsScreen";
-import EventScreen from "./pages/EventScreen/EventScreen";
-import MainScreen from "./pages/MainScreen/MainScreen";
-import ProductCardScreen from "./pages/ProductCardScreen/ProductCardScreen";
-import ProfileScreen from "./pages/ProfileScreen/ProfieScreen";
-import ProgressScreen from "./pages/ProgressScreen/ProgressScreen";
-import RecipesScreen from "./pages/RecipesScreen/RecipesScreen";
-import CategoriesRecipesScreen from "./pages/CategoriesRecipesScreen/CategoriesRecipesScreen";
-import SettingScreen from "./pages/SettingScreen/SettingScreen";
+// import RecipeScreen from "./pages/RecipeScreen/RecipeScreen";
+// import RegistrationScreen from "./pages/RegistrationScreen/RegistrationScreen";
+// import AboutUsScreen from "./pages/AboutUsScreen/AboutUsScreen";
+// import EventScreen from "./pages/EventScreen/EventScreen";
+// import MainScreen from "./pages/MainScreen/MainScreen";
+// import ProductCardScreen from "./pages/ProductCardScreen/ProductCardScreen";
+// import ProfileScreen from "./pages/ProfileScreen/ProfieScreen";
+// import ProgressScreen from "./pages/ProgressScreen/ProgressScreen";
+// import RecipesScreen from "./pages/RecipesScreen/RecipesScreen";
+// import CategoriesRecipesScreen from "./pages/CategoriesRecipesScreen/CategoriesRecipesScreen";
+// import SettingScreen from "./pages/SettingScreen/SettingScreen";
+import LoginScreem from "./pages/LoginScreen/LoginScreen";
 
 const App: React.FC = () => {
   return (
     <>
       <Header />
       <main className="main-app">
-        <AboutUsScreen />
+        {/* <AboutUsScreen />
         <MainScreen />
         <EventScreen type="breakfast" />
         <RecipeScreen />
@@ -28,7 +29,8 @@ const App: React.FC = () => {
         <ProductCardScreen />
         <ProfileScreen />
         <ProgressScreen />
-        <RegistrationScreen />
+        <RegistrationScreen /> */}
+        <LoginScreem />
       </main>
       <Footer />
     </>
