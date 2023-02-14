f9d02e24e0afcbcf498114cedb27f4266e223287
asya2320 <64478347+asya2320@users.noreply.github.com>
2023-02-15 00:48:26 +0300

    fix: hearts to main page recipes

--- src/components/CardRecipe/CardRecipe.tsx
+++ src/components/CardRecipe/CardRecipe.tsx
@@ -29,7 +29,9 @@ const CardRecipe: React.FC<CardRecipesProps> = ({ data }) => {
         className="recipe__card__img"
         style={{ backgroundImage: `url(${data.imageURL})` }}
       >
-        <div className="recipe__card__like" />
+        <div className="recipe__card__bg__like">
+          <div className="recipe__card__like" />
+        </div>
       </div>
       <div className="recipe__card__wrapper">
         <h3 className="recipe__card__h3">{data.name}</h3>
