f4d8db22b53a589fe41c4880167e1292c8366734
asya2320 <64478347+asya2320@users.noreply.github.com>
2023-02-16 01:23:13 +0300

    feat/add backround color on edit event screen and plus button to water

--- src/pages/EditEventsScreen/EditEventScreen.tsx
+++ src/pages/EditEventsScreen/EditEventScreen.tsx
@@ -48,57 +48,59 @@ const EditEventScreen = () => {
   ]; // будем получать эти данные из бд
 
   return (
-    <div className="container">
-      <div className="daily-events">
-        <div className="daily-events__meals">
-          <h1 className="daily-events__title">Daily meals</h1>
-          <DailyEventEditWrapper
-            title="Breakfast"
-            recommended="Recomended 447 Kcal"
-            quantity="356 kcal"
-            content={<DailyEventEditData type="meal" data={breakfast} />}
-            className="daily-events__item daily-events__item_breakfast"
-          />
-          <DailyEventEditWrapper
-            title="Lunch"
-            recommended="Recomended 447 Kcal"
-            className="daily-events__item daily-events__item_lunch"
-            content={<DailyEventEditData type="meal" data={lunch} />}
-          />
-          <DailyEventEditWrapper
-            title="Dinner"
-            recommended="Recomended 447 Kcal"
-            className="daily-events__item daily-events__item_dinner"
-            content={<DailyEventEditData type="meal" data={dinner} />}
-          />
-          <DailyEventEditWrapper
-            title="Snack"
-            recommended="Recomended 447 Kcal"
-            className="daily-events__item daily-events__item_snack"
-            content={<DailyEventEditData type="meal" data={snaks} />}
-          />
+    <div className="edit-event__screen">
+      <div className="container">
+        <div className="daily-events">
+          <div className="daily-events__meals">
+            <h1 className="daily-events__title">Daily meals</h1>
+            <DailyEventEditWrapper
+              title="Breakfast"
+              recommended="Recomended 447 Kcal"
+              quantity="356 kcal"
+              content={<DailyEventEditData type="meal" data={breakfast} />}
+              className="daily-events__item daily-events__item_breakfast"
+            />
+            <DailyEventEditWrapper
+              title="Lunch"
+              recommended="Recomended 447 Kcal"
+              className="daily-events__item daily-events__item_lunch"
+              content={<DailyEventEditData type="meal" data={lunch} />}
+            />
+            <DailyEventEditWrapper
+              title="Dinner"
+              recommended="Recomended 447 Kcal"
+              className="daily-events__item daily-events__item_dinner"
+              content={<DailyEventEditData type="meal" data={dinner} />}
+            />
+            <DailyEventEditWrapper
+              title="Snack"
+              recommended="Recomended 447 Kcal"
+              className="daily-events__item daily-events__item_snack"
+              content={<DailyEventEditData type="meal" data={snaks} />}
+            />
+          </div>
+          <div className="daily-events__water">
+            <h3 className="daily-events__title">Water consumed</h3>
+            <DailyEventEditWrapper
+              title="Water"
+              quantity="0.75L (40%)"
+              recommended="Recomended 2.0L (8 gls)"
+              className="daily-events__item daily-events__item_water"
+              content={getWaterConsumed()}
+            />
+          </div>
+          <div className="daily-events__exercise">
+            <h3 className="daily-events__title">Daily exercise</h3>
+            <DailyEventEditWrapper
+              title="Exercise"
+              recommended="Last: Run 1 km"
+              className="daily-events__item daily-events__item_exercise"
+              content={<DailyEventEditData type="activity" data={activities} />}
+            />
+          </div>
         </div>
-        <div className="daily-events__water">
-          <h3 className="daily-events__title">Water consumed</h3>
-          <DailyEventEditWrapper
-            title="Water"
-            quantity="0.75L (40%)"
-            recommended="Recomended 2.0L (8 gls)"
-            className="daily-events__item daily-events__item_water"
-            content={getWaterConsumed()}
-          />
-        </div>
-        <div className="daily-events__exercise">
-          <h3 className="daily-events__title">Daily exercise</h3>
-          <DailyEventEditWrapper
-            title="Exercise"
-            recommended="Last: Run 1 km"
-            className="daily-events__item daily-events__item_exercise"
-            content={<DailyEventEditData type="activity" data={activities} />}
-          />
-        </div>
+        <div className="event__changes__btn">Save changes</div>
       </div>
-      <div className="event__changes__btn">Save changes</div>
     </div>
   );
 };
