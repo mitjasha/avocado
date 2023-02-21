239a055f995077f57e6d174cf056d74fb3583fef
ViktoriaKondrashova1 <kavidmi@gmail.com>
2023-02-21 22:19:09 +0300

    feat: add update modal data fn

--- src/containers/ProductModal/ProductModal.tsx
+++ src/containers/ProductModal/ProductModal.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import BasicModalComponent from "../../components/Modals/BasicModalComponent/BasicModalComponent";
 import RegInput from "../../components/Inputs/BaseInput/BaseInput";
 import ButtonTemplate from "../../components/Buttons/ButtonTemplate/ButtonTemplate";
@@ -19,28 +19,51 @@ interface ProductProps {
 }
 
 const ProductModal: React.FC<ProductProps> = ({ data }) => {
+  const [kcal, setKcal] = useState<number>(Number(data.kcal));
+  const [proteins, setProteins] = useState<number>(Number(data.proteins));
+  const [fats, setFats] = useState<number>(Number(data.fats));
+  const [carbs, setCarbs] = useState<number>(Number(data.carbs));
+
+  const updateModalData = () => {
+    const modalInput = document.querySelector(
+      ".modal__input",
+    ) as HTMLInputElement;
+    setKcal(Math.round((Number(modalInput.value) / 100) * Number(data.kcal)));
+    setProteins(
+      Math.round((Number(modalInput.value) / 100) * Number(data.proteins)),
+    );
+    setFats(Math.round((Number(modalInput.value) / 100) * Number(data.fats)));
+    setCarbs(Math.round((Number(modalInput.value) / 100) * Number(data.carbs)));
+  };
+
   return (
     <BasicModalComponent title={data.namEng} className="product-modal">
       <div className="modal__container">
         <div>
-          <RegInput className="modal__input" type="number" placeholder="100" />
+          <RegInput
+            className="modal__input"
+            type="number"
+            placeholder="100"
+            defaultValue={100}
+            onChange={updateModalData}
+          />
           <span className="modal__span">&nbsp;g</span>
         </div>
         <div className="modal__arrow-icon" />
-        <div className="modal__span__disabled">{data.kcal} kcal</div>
+        <div className="modal__span__disabled">{kcal} kcal</div>
       </div>
       <div className="modal__nutritions">
         <div className="modal__container">
           <span className="modal__span">Proteins</span>
-          <div className="modal__span__disabled">{data.proteins} g</div>
+          <div className="modal__span__disabled">{proteins} g</div>
         </div>
         <div className="modal__container">
           <span className="modal__span">Fats</span>
-          <div className="modal__span__disabled">{data.fats} g</div>
+          <div className="modal__span__disabled">{fats} g</div>
         </div>
         <div className="modal__container">
           <span className="modal__span">Carbs</span>
-          <div className="modal__span__disabled">{data.carbs} g</div>
+          <div className="modal__span__disabled">{carbs} g</div>
         </div>
       </div>
       <ButtonTemplate className="modal__button">Add</ButtonTemplate>
