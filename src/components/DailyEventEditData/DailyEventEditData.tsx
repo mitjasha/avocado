017b0369719eba1b7aa8051e4422374d0c4b9330
asya2320 <64478347+asya2320@users.noreply.github.com>
2023-02-24 02:45:16 +0300

    add update requests to activity and meals

--- src/components/DailyEventEditData/DailyEventEditData.tsx
+++ src/components/DailyEventEditData/DailyEventEditData.tsx
@@ -1,13 +1,15 @@
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable no-unneeded-ternary */
-import React from "react";
+import React, { useEffect, useState } from "react";
 import RegInput from "../Inputs/BaseInput/BaseInput";
 import {
   EventActivityResponse,
   EventMealResponse,
 } from "../../api/api.interface";
 import "./DailyEventEditData.scss";
+import eventMealController from "../../api/event-meal.controller";
+import eventActivityController from "../../api/event-activity.controller";
 
 export interface DailyEventData {
   type: string;
@@ -15,14 +17,48 @@ export interface DailyEventData {
   dataActivity?: EventActivityResponse[];
 }
 
+const updateMealEvent = async (
+  id: string,
+  startTime: string,
+  weight: number,
+  description: string,
+) => {
+  await eventMealController.updateEvent({ id, startTime, weight, description });
+};
+
+const updateActivityEvent = async (
+  id: string,
+  startTime: Date,
+  endTime: Date,
+  description: string,
+) => {
+  await eventActivityController.updateEvent({
+    id,
+    startTime,
+    endTime,
+    description,
+  });
+};
+
 export const getMinutes = (start: Date, end: Date) =>
   new Date(end).getMinutes() - new Date(start).getMinutes();
+
+export const getEndTime = (start: Date, minutes: number) =>
+  new Date(new Date(start).getMinutes() + minutes);
 
 const DailyEventEditData: React.FC<DailyEventData> = ({
   type,
   dataMeal,
   dataActivity,
 }) => {
+  const [weight, setWeight] = useState<number>();
+  const [timeEnd, setEndTime] = useState<Date>();
+
+  useEffect(() => {}, [
+    [weight, setWeight],
+    [timeEnd, setEndTime],
+  ]);
+
   return (
     <ul className="container__info__ul">
       {type === "meal"
@@ -35,13 +71,27 @@ const DailyEventEditData: React.FC<DailyEventData> = ({
                     <RegInput
                       type="number"
                       placeholder={String(item.weight)}
+                      value={weight ? String(weight) : String(item.weight)}
                       className="info__input"
+                      onChange={(event) => {
+                        setWeight(
+                          Number((event.target as HTMLSelectElement).value),
+                        );
+                        updateMealEvent(
+                          item.product.id,
+                          item.startTime,
+                          weight ? weight : item.weight,
+                          item.description,
+                        );
+                      }}
                     />
                     <span>g</span>
                   </div>
                 </div>
                 <span className="info__name">
-                  {(item.weight / 100) * item.product.calories_100g} kcal
+                  {(weight ? weight / 100 : item.weight / 100) *
+                    item.product.calories_100g}{" "}
+                  kcal
                 </span>
               </div>
               <span className="info__delete">delete</span>
@@ -58,13 +108,34 @@ const DailyEventEditData: React.FC<DailyEventData> = ({
                       placeholder={String(
                         getMinutes(item.startTime, item.endTime),
                       )}
+                      value={String(
+                        timeEnd
+                          ? getMinutes(item.startTime, timeEnd)
+                          : getMinutes(item.startTime, item.endTime),
+                      )}
                       className="info__input"
+                      onChange={(event) => {
+                        setEndTime(
+                          getEndTime(
+                            item.startTime,
+                            Number((event.target as HTMLSelectElement).value),
+                          ),
+                        );
+                        updateActivityEvent(
+                          item.activity.id,
+                          item.startTime,
+                          timeEnd ? timeEnd : item.endTime,
+                          item.description,
+                        );
+                      }}
                     />
                     <span>min</span>
                   </div>
                 </div>
                 <span className="info__name">
-                  {getMinutes(item.startTime, item.endTime) *
+                  {(timeEnd
+                    ? getMinutes(item.startTime, timeEnd)
+                    : getMinutes(item.startTime, item.endTime)) *
                     item.activity.calories_per_min}{" "}
                   min
                 </span>
