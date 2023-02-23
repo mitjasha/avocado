a421046ccd2e20cfaa4b01ea84a2cdc30545522e
asya2320 <64478347+asya2320@users.noreply.github.com>
2023-02-23 14:16:11 +0300

    fix validation to from add product

--- src/containers/AddProductModal/AddProductModal.tsx
+++ src/containers/AddProductModal/AddProductModal.tsx
@@ -15,6 +15,7 @@ const AddProductModal: React.FC = () => {
     "Carbs",
   ];
   const productCategories = [
+    "",
     "dairy",
     "pastries",
     "oils",
@@ -59,102 +60,120 @@ const AddProductModal: React.FC = () => {
   const [proteins, setProteins] = useState("");
   const [fats, setFats] = useState("");
   const [carbs, setCarbs] = useState("");
-  const [category, setCategory] = useState("dairy");
+  const [category, setCategory] = useState("");
 
   return (
     <BasicModalComponent title="Add new product" className="add-product-modal">
       <div className="add__containter">
-        <div className="add__param">
-          {addProdutFields.map((item) => {
-            return <p key={item}>{`${item}:`}</p>;
-          })}
-        </div>
-        <div className="add__values">
-          <select
-            className="add__select"
-            value={category}
-            onChange={(event) =>
-              setCategory((event.target as HTMLSelectElement).value)
-            }
-          >
-            {productCategories.map((item) => {
-              return <option key={item}>{item}</option>;
+        <form
+          style={{
+            display: "flex",
+            flexWrap: "wrap",
+            justifyContent: "space-between",
+          }}
+        >
+          <div className="add__param">
+            {addProdutFields.map((item) => {
+              return <p key={item}>{`${item}:`}</p>;
             })}
-          </select>
-          <RegInput
-            className="add__input"
-            type="text"
-            placeholder="Enter a name"
-            value={name}
-            onChange={(event) =>
-              setName((event.target as HTMLInputElement).value)
-            }
-          />
-          <div className="add__input__container">
+          </div>
+          <div className="add__values">
+            <select
+              required
+              className="add__select"
+              value={category}
+              onChange={(event) =>
+                setCategory((event.target as HTMLSelectElement).value)
+              }
+            >
+              {productCategories.map((item) => {
+                return <option key={item}>{item}</option>;
+              })}
+            </select>
             <RegInput
               className="add__input"
-              type="number"
-              placeholder="Kcal per 100 g"
-              value={kcal}
+              type="text"
+              placeholder="Enter a name"
+              value={name}
               onChange={(event) =>
-                setKcal((event.target as HTMLInputElement).value)
+                setName((event.target as HTMLInputElement).value)
               }
             />
-            <span>&nbsp;kcal</span>
+            <div className="add__input__container">
+              <RegInput
+                className="add__input"
+                type="number"
+                placeholder="Kcal per 100 g"
+                value={kcal}
+                onChange={(event) =>
+                  setKcal((event.target as HTMLInputElement).value)
+                }
+              />
+              <span>&nbsp;kcal</span>
+            </div>
+            <div className="add__input__container">
+              <RegInput
+                className="add__input"
+                type="number"
+                placeholder="Proteins per 100 g"
+                value={proteins}
+                onChange={(event) =>
+                  setProteins((event.target as HTMLInputElement).value)
+                }
+              />
+              <span>&nbsp;g</span>
+            </div>
+            <div className="add__input__container">
+              <RegInput
+                className="add__input"
+                type="number"
+                placeholder="Fats per 100 g"
+                value={fats}
+                onChange={(event) =>
+                  setFats((event.target as HTMLInputElement).value)
+                }
+              />
+              <span>&nbsp;g</span>
+            </div>
+            <div className="add__input__container">
+              <RegInput
+                className="add__input"
+                type="number"
+                placeholder="Carbs per 100 g"
+                value={carbs}
+                onChange={(event) =>
+                  setCarbs((event.target as HTMLInputElement).value)
+                }
+              />
+              <span>&nbsp;g</span>
+            </div>
           </div>
-          <div className="add__input__container">
-            <RegInput
-              className="add__input"
-              type="number"
-              placeholder="Proteins per 100 g"
-              value={proteins}
-              onChange={(event) =>
-                setProteins((event.target as HTMLInputElement).value)
-              }
-            />
-            <span>&nbsp;g</span>
-          </div>
-          <div className="add__input__container">
-            <RegInput
-              className="add__input"
-              type="number"
-              placeholder="Fats per 100 g"
-              value={fats}
-              onChange={(event) =>
-                setFats((event.target as HTMLInputElement).value)
-              }
-            />
-            <span>&nbsp;g</span>
-          </div>
-          <div className="add__input__container">
-            <RegInput
-              className="add__input"
-              type="number"
-              placeholder="Carbs per 100 g"
-              value={carbs}
-              onChange={(event) =>
-                setCarbs((event.target as HTMLInputElement).value)
-              }
-            />
-            <span>&nbsp;g</span>
-          </div>
-        </div>
+          <ButtonTemplate
+            type="submit"
+            className="modal__button"
+            onClick={() => {
+              if (
+                name !== "" &&
+                kcal !== "" &&
+                proteins !== "" &&
+                carbs !== "" &&
+                fats !== "" &&
+                category !== ""
+              )
+                addProductRequest(
+                  name,
+                  Number(kcal),
+                  Number(proteins),
+                  Number(carbs),
+                  Number(fats),
+                  category,
+                );
+            }}
+          >
+            Add
+          </ButtonTemplate>
+        </form>
       </div>
-      <ButtonTemplate
-        className="modal__button"
-        onClick={() => {
-          addProductRequest(
-            name,
-            Number(kcal),
-            Number(proteins),
-            Number(carbs),
-            Number(fats),
-            category,
-          );
-        }}
-      >
-        Add
-      </ButtonTemplate>
     </BasicModalComponent>
   );
 };
