4f1a07603394470cbc9ea817ee3b72645a4bd278
asya2320 <64478347+asya2320@users.noreply.github.com>
2023-02-24 19:08:41 +0300

    add translate about us screen

--- src/pages/AboutUsScreen/AboutUsScreen.tsx
+++ src/pages/AboutUsScreen/AboutUsScreen.tsx
@@ -1,41 +1,29 @@
 import React from "react";
 import "./AboutUsScreen.scss";
 import "../../index.scss";
+import { useTranslation } from "react-i18next";
 
 const AboutUsScreen = () => {
+  const { t } = useTranslation();
   return (
     <div className="about__us__screen">
       <div className="container">
-        <h1 className="about__us__title">About us</h1>
+        <h1 className="about__us__title">{t("about_us_title")}</h1>
         <div className="about__us__logo-wrapper">
           <div className="about__us__logo" />
-          <h3 className="about__us__top_h3">WHO WE ARE?</h3>
+          <h3 className="about__us__top_h3">{t("about_us_header")}</h3>
         </div>
         <div className="about__us__container">
-          <span className="about__us__text">
-            Our team is a big friendly family, which is pleased to present our
-            project that improves people’s lives. We know how difficult it is to
-            change your lifestyle. That’s why it was important for us to create
-            such an application, opening which you would like to come back
-            again.
-          </span>
-          <h3 className="about__us__text__h3">
-            WHAT WILL YOU GET USING OUR APP?
-          </h3>
+          <span className="about__us__text">{t("about_us_text")}</span>
+          <h3 className="about__us__text__h3">{t("about_us_question")}</h3>
           <ul className="about__us__ul">
-            <li className="about__us__text">Control of calories consumed</li>
-            <li className="about__us__text">
-              Control of consumed proteins, fats and carbs
-            </li>
-            <li className="about__us__text">Recipes for proper nutrition</li>
-            <li className="about__us__text">Control of the water consumed</li>
-            <li className="about__us__text">Monitoring activity for the day</li>
+            <li className="about__us__text">{t("about_us_li_1")}</li>
+            <li className="about__us__text">{t("about_us_li_2")}</li>
+            <li className="about__us__text">{t("about_us_li_3")}</li>
+            <li className="about__us__text">{t("about_us_li_4")}</li>
+            <li className="about__us__text">{t("about_us_li_5")}</li>
           </ul>
-          <span className="about__us__text">
-            Start today! Taking care of yourself is easier than it seems. We
-            wish you success in achieving your goal on the path of a healthy
-            lifestyle!
-          </span>
+          <span className="about__us__text">{t("about_us_end_text")}</span>
           <div className="about__us__heart" />
         </div>
       </div>
